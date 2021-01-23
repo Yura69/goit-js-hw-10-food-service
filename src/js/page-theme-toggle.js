1863026f0f615b6417ac123ece4db45389b6ddce
Yura Dziubenko <68082402+Yura69@users.noreply.github.com>
2021-01-23 13:26:38 +0200

    вносит все правки от Ментора
    внёс исправления.

--- src/js/page-theme-toggle.js
+++ src/js/page-theme-toggle.js
@@ -1,27 +1,28 @@
 const theme = {
-    LIGHT: 'light-theme',
-    DARK: 'dark-theme',
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
 };
 
 const checkboxRef = document.querySelector('.theme-switch__toggle');
 
 if (localStorage.getItem('DARK') !== null) {
-    document.body.classList.add(localStorage.getItem('DARK'));
-    checkboxRef.checked = true;
+  document.body.classList.add(theme.DARK);
+  checkboxRef.checked = true;
 } else {
-    checkboxRef.checked = false;
-};
+  checkboxRef.checked = false;
+  document.body.classList.add(theme.LIGHT);
+}
 
 checkboxRef.addEventListener('change', () => {
-    if (checkboxRef.checked === true) {
-        document.body.classList.add('dark-theme');
-        document.body.classList.remove('light-theme');
-        localStorage.setItem('DARK', theme.DARK);
-        localStorage.removeItem('LIGHT', theme.LIGHT);
-    } else {
-        document.body.classList.add('light-theme');
-        document.body.classList.remove('dark-theme');
-        localStorage.setItem('LIGHT', theme.LIGHT);
-        localStorage.removeItem('DARK', theme.DARK);
-    }
+  if (checkboxRef.checked === true) {
+    document.body.classList.add('dark-theme');
+    document.body.classList.remove('light-theme');
+    localStorage.setItem('theme', 'DARK');
+    localStorage.removeItem('theme', 'LIGHT');
+  } else {
+    document.body.classList.add('light-theme');
+    document.body.classList.remove('dark-theme');
+    localStorage.setItem('theme', 'LIGHT');
+    localStorage.removeItem('theme', 'DARK');
+  }
 });
