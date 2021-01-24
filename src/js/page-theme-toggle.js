0c2143340e682d54d73fee04efd3d7a7cdaf1bf5
Yura Dziubenko <68082402+Yura69@users.noreply.github.com>
2021-01-24 23:03:51 +0200

    1
    1

--- src/js/page-theme-toggle.js
+++ src/js/page-theme-toggle.js
@@ -4,6 +4,7 @@ const theme = {
 };
 
 const checkboxRef = document.querySelector('.theme-switch__toggle');
+console.log('Проверка на измененния кода рабой страницы в Гите');
 if (localStorage.value === 'DARK') {
   checkboxRef.checked = true;
   document.body.classList.add(theme.DARK);
